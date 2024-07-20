25d16e85c9042c44f4c3318243d0c6bf78eae61f
konstantin-palikarsky <e11924413@student.tuwien.ac.at>
2024-07-20 11:26:04 +0200

    First steps

---
+++ go.mod
@@ -0,0 +1,6 @@
+module tuwien.ac.at/calculator/v2
+
+go 1.22.5
+
+require "tuwien.ac.at/calculator/streams" v0.0.0
+replace "tuwien.ac.at/calculator/streams" v0.0.0 => "./src/streams"
--- src/Calculator.go
+++ src/Calculator.go
@@ -1,7 +1,27 @@
 package main
 
 import "fmt"
+import "tuwien.ac.at/calculator/v2/src/streams"
+
+var registerSet = make(map[rune]interface{})
+var operationMode int64
+var dataStack = make([]interface{}, 8)
+
+var commandStream = streams.NewStream()
+var inputStream = streams.NewStream()
+var outputStream = streams.NewStream()
+
+func init() {
+	operationMode = 0
+
+	//runs once before main, should use to set the registers
+	registerSet['a'] = "firstCommand"
+}
 
 func main() {
-	fmt.Printf("%s", "hi world")
+	fmt.Println("hi world", operationMode)
+	commandStream.AddToBack("lastCommand")
+	commandStream.AddToFront("firstCommand")
+
+	fmt.Println(commandStream)
 }
